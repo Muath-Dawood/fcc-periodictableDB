ad5ca2ffb7b4af2d100d2a02441e91ef86da3ce2
Muath Dawood <muath.dawood.89@gmail.com>
2024-05-04 21:36:41 +0000

    feat: run query when using argument

--- element.sh
+++ element.sh
@@ -5,4 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
+else
+  ELEMENT=$($PSQL "select * from elements inner join properties using (atomic_number) inner join types using (type_id) where atomic_number = $1 or symbol = '$1' or name = '$1';")
+  echo $ELEMENT
 fi
